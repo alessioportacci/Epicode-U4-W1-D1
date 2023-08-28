6c780ac497de1ef7950d34b1720422c821a7013f
Alessio <alessio.portacci.s@gmail.com>
2023-08-28 12:58:47 +0200

    Aggiungere i file di progetto.

---
+++ Epicode-U4-W1-D1/Autovetture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epicode_U4_W1_D1
+{
+    internal class Autovetture
+    {
+        private string _marca;
+
+        public string Marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        } 
+    }
+
+    public class Autovetture2
+    {
+        public string Marca { get; set; }
+    }
+}
---
+++ Epicode-U4-W1-D1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Epicode_U4_W1_D1;
+
+
+namespace Epicode_U4_W1_D1
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        { 
+            Autovetture auto1 = new Autovetture();
+            auto1.Marca = "BMW";
+            Console.WriteLine(auto1.Marca);
+
+
+            Autovetture2 auto2 = new Autovetture2();
+            auto2.Marca = "Fiat";
+            Console.WriteLine(auto2.Marca) ;
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
